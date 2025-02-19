393053a3263a01abc3aee0c4361be24a1349c5ac
Eduardo Henrique <itsduzao@outlook.com>
2025-02-19 11:42:44 -0300

    feat: add menu handle click function

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import { MENU_ITEMS } from './src/data/data.js'
 
+let orderItems = {}
+
 function getMenuItemsHTML(dataArr) {
   const htmlContent = []
 
   dataArr.map(item => {
     const { name, ingredients, id, price, emoji } = item
-    htmlContent.push(`<div class="item-container" role="listitem" data-id="${id}">
+    htmlContent.push(`<div class="item-container" role="listitem">
   <div class="item-img" role="img" aria-label="${name}">${emoji}</div>
   <div class="item-info" role="group">
     <div class="item-title">${name}</div>
     <div class="item-description">${ingredients.join(', ')}</div>
     <div class="item-price">$${price}</div>
   </div>
-  <button class="btn-add" type="button">
+  <button class="btn-add" type="button" data-id="${id}">
     +
   </button>
 </div>`)
@@ -25,4 +27,27 @@ function renderMenuItems() {
   document.getElementById('app-menu').innerHTML = getMenuItemsHTML(MENU_ITEMS)
 }
 
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('btn-add') && event.target.dataset.id) {
+    handleMenuItemClick(event)
+  }
+})
+
 renderMenuItems()
+
+function handleMenuItemClick(event) {
+  const targetItem = MENU_ITEMS.filter(item => {
+    return item.id === Number(event.target.dataset.id)
+  })[0]
+
+  !orderItems[`${targetItem.id}`]
+    ? (orderItems[`${targetItem.id}`] = 1)
+    : (orderItems[`${targetItem.id}`] += 1)
+
+  const orderCheckoutElement = document.querySelector('#order-checkout')
+
+  if (orderCheckoutElement.classList.contains('hidden')) {
+    orderCheckoutElement.classList.remove('hidden')
+  }
+}
+
